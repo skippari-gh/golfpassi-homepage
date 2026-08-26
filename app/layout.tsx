8f65906dcd4802c4425189d1900e57cc93a70871
skippari-gh <skippari@gmail.com>
2026-08-25 22:31:55 -0700

    Enable map zoom controls

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import MapZoom from "./MapZoom";
 
 export const metadata: Metadata = {
   title: "Golfpassi maailmalla",
@@ -9,7 +10,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fi">
-      <body>{children}</body>
+      <body>
+        {children}
+        <MapZoom />
+      </body>
     </html>
   );
 }
